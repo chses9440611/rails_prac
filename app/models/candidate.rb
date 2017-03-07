f7b6bb046dc41f01c3eae9669f11174ec5af4574
Jason Huang <chses910372@gmail.com>
2017-03-07 20:19:59 +0800

    Add validation

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,10 @@
 class Candidate < ApplicationRecord
+	validates :name, presence:true
+
+	private
+	def name_validator
+		unless name.start_with? 'Hello'
+			errors[:name] << "Have to start with Hello"
+		end
+	end
 end
