602b930810e32aa7fa635548dce75dc8d31dafa7
Jason Huang <chses910372@gmail.com>
2017-03-07 20:29:46 +0800

    Add self-def validation

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,9 +1,10 @@
 class Candidate < ApplicationRecord
-	validates :name, presence:true
+	#validates :name, presence:true
+	validate :name_validator
 
 	private
 	def name_validator
-		unless name.start_with? 'Hello'
+		unless name.starts_with? 'Hello'
 			errors[:name] << "Have to start with Hello"
 		end
 	end
